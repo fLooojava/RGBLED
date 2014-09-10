a86e415798e6fed55d07cf62f230782cc8092885
mathis.florian <mathis.florian@gmx.at>
2014-09-10 17:56:22 +0200

    pushed the intent values

--- src/com/example/rgbled/SettingsActivity.java
+++ src/com/example/rgbled/SettingsActivity.java
@@ -48,6 +48,7 @@ public class SettingsActivity extends Activity
 	                mainScreen.putExtra("macaddress", mac);
 	                // Intent starten und zur zweiten Activity wechseln
 	                startActivity(mainScreen);
+	                
 				}
 			});
 	       
